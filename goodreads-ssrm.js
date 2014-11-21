08f207ba8d2c39c7da7d52b6b9ae59e5d374ea27
Bill DePhillips <bill.dephillips@gmail.com>
2014-11-20 17:54:38 -0800

    hot damn it works.

--- goodreads-ssrm.js
+++ goodreads-ssrm.js
@@ -1,45 +1,35 @@
 // goodreads SSRM (server-side rendering middleware)
 'use strict';
 
-var wait = require('wait.for');
 var fs = require('fs');
-var jsdom = require('jsdom');
+var wait = require('wait.for');
+var phantom = require('node-phantom-simple');
+var path = require('path');
 
-var jsDomGet = function(appFilename, url, callback) {
-  jsdom.env({
-    file: appFilename,
-    windowLocation: url,
-    features: {
-      QuerySelector: true
-    },
-    created: function(errors, window) {
-      // angular needs this but jsdom doesn't provide it
-      window.scrollTo = function() {};
+var goodreadsState = {};
 
-      window.history.pushState(null, null, url);
-    }
+var syncPhantomRender = function(phantomRuntime, appFilename, url, callback) {
+  var start = new Date();
+  phantomRuntime.createPage(function(err, page) {
+    var appFileUrl = 'http://localhost:9000' + url;
+    console.log('[DEBUG] appFileUrl: ' + appFileUrl);
+    page.open(appFileUrl, function(err, status) {
+    console.log('[GR-SSRM] phantom rendering consumed ' + ((new Date()) - start) + 'ms')
+      page.evaluate(function(url) {
+          return document.documentElement.outerHTML;
+        },
+        function(err, result) {
+        callback(err, result);
+      });
+    });
   });
 };
 
-var renderFileUsingJsDom = function(appFilename, url) {
-  var start = Date.now();
-  var window = wait.for(jsDomGet, appFilename, url);
-  var end = Date.now();
-  console.log('[GRSSRM] jsdom took: ' + (end - start) + 'ms');
-  return window.document.querySelector('html').innerHTML;
-};
-
-var serverSideRender = function(appFilename, req, res) {
-  try {
-    var doc = renderFileUsingJsDom(appFilename, req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(doc);
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.end('<html><head><title>:-(</title></head><body><img src="http://tailf.blob.core.windows.net/cats/500.jpg"/></body></html>');
-  }
-};
+var serverSideRender = function(phantomRuntime, appFilename, req, res) {
+  var html = wait.for(syncPhantomRender, phantomRuntime, appFilename, req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(html);
+}
 
 var isAngularRequest = function(url) {
   return url.match(/\./) === null;
@@ -47,24 +37,46 @@ var isAngularRequest = function(url) {
 
 var middlewareFactory = function(appFilename, options) {
 
-  if (options !== undefined && options.skipSsr === true) {
-    return function(req, res, next) {
-        if (isAngularRequest(req.url)) {
-          console.log('[GR-SSRM] skipped SSR. just sending ' + appFilename);
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(fs.readFileSync(appFilename));
-          res.end();
-        } else {
-          next();
-        }
+  var phantomRuntime;
+
+  var start = new Date();
+  phantom.create(function(err, newRuntime) {
+    if (err) {
+      throw new Error(err);
     }
+    console.log('phantom runtime creation consumed ' + ((new Date()) - start) + 'ms');
+    phantomRuntime = newRuntime;
+  });
+
+  if (options !== undefined && options.skipSsr === true) {
+    throw new Error('temporarily disabled');
   }
 
   return function(req, res, next) {
     if (isAngularRequest(req.url)) {
-      console.log('[GR-SSRM] server-side rendering ng-app ' + appFilename + ' with route ' + req.url);
-      wait.launchFiber(serverSideRender, appFilename, req, res);
+
+      if (phantomRuntime === undefined) {
+        console.log('[GR-SSRM] cannot yet render pages. phantom runtime not yet available.');
+        res.writeHead(503, {'Content-Type': 'text/html'});
+        res.end('<html><head><title>:-|</title></head><body><h1>come back later</h1>' +
+                '<img src="https://c4.staticflickr.com/8/7157/6540643319_7945715c3a_n.jpg" /></body></html>');
+      } else {
+
+        var userAgent = req.headers['user-agent'];
+        console.log('[DEBUG] userAgent: ' + userAgent);
+        if (userAgent.match('PhantomJS')) {
+          console.log('[GR-SSRM] sending ' + appFilename + ' to phantom pre-rendering');
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(fs.readFileSync(appFilename));
+          res.end();
+        } else {
+          console.log('[GR-SSRM] server-side rendering appFile ' + appFilename + ' with route ' + req.url);
+          wait.launchFiber(serverSideRender, phantomRuntime, appFilename, req, res);
+        }
+      }
+
     } else {
+      console.log('[GR-SSRM] serving static asset: ' + req.url);
       next();
     }
   };
